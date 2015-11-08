aa8ccc39fcb270ee2073e3cf6e02df3092751928
Alex Hoffnung <alex.hoffnung@gmail.com>
2015-11-08 18:12:15 -0500

    Add update surname functionality to user profiles. Prepare to integrate Meteor Profile

--- client/user/user.js
+++ client/user/user.js
@@ -1,11 +1,39 @@
 if (Meteor.isClient) {
-  // This code only runs on the client	
+	// This code only runs on the client
+	Meteor.subscribe("currentUserData");
+
 	Template.user.helpers({
 	  firstName: function() {
 	    return Meteor.userId();
 	  },
 	  lastName: function() {
-	    return Meteor.users.find();
+	  	var tempName = Meteor.users.find( { owner:Meteor.userId() } );
+	  	console.log(tempName);
+	    return Meteor.users.find( {_id:Meteor.userId() } ).profile.surname;
 	  }
 	});
+
+
+	Template.user.events({
+    "submit .update-user-surname": function (event) {
+      // Prevent default browser form submit
+      event.preventDefault();
+
+      // Get current user id
+      var currentUserId = Meteor.userId();
+ 
+      // Get value from form element
+      var text = event.target.text.value;
+ 
+      // Insert a task into the collection
+      Meteor.users.update({_id:Meteor.userId()}, { $set: {'profile.surname':text} });
+      console.log(Meteor.users.find().fetch());
+console.log(text);
+var tempName = Meteor.users.find( { _id:Meteor.userId() } );
+console.log(tempName);
+ 
+      // Clear form
+      event.target.text.value = "";
+    }
+  });
 }
--- server/server.js
+++ server/server.js
@@ -1,5 +1,13 @@
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+ 	Meteor.startup(function () {
+    	// code to run on server at startup
+  	});
+
+  	Accounts.onCreateUser(function(options, user) {
+    //pass the surname in the options
+
+    user.profile['surname'] = options.surname;
+
+    return user;
+	});
 }

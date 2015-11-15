834c57151e1f80653cb1c9d380d13ef08b00c060
Alex Hoffnung <alex.hoffnung@gmail.com>
2015-11-14 19:54:37 -0500

    Add texture to gotbaby-wall

--- client/charts/charts.js
+++ client/charts/charts.js
@@ -1,13 +1,39 @@
 if (Meteor.isClient) {
   // This code only runs on the client
 Template.charts.onRendered(function () {
+    // Get current user id
+    var currentUserId = Meteor.userId();
+    
+    var data = {
+    labels: ['wet', 'dirty'],
+    series: [
+        Diapers.find( { $and: [ {checked: {$ne: true}}, { owner:currentUserId } ] } ).count(),
+        Diapers.find( { $and: [ {checked: {$ne: false}}, { owner:currentUserId } ] } ).count()
+    ]
+    };
 
-    new Chartist.Bar('.ct-chart', {
-      labels: ['wet', 'dirty'],
-      series: [20, 60]
-    }, {
-      distributeSeries: true
+    var options = {
+      labelInterpolationFnc: function(value) {
+        return value[0]
+      }
+    };
+
+    var responsiveOptions = [
+      ['screen and (min-width: 640px)', {
+        chartPadding: 30,
+        labelOffset: 100,
+        labelDirection: 'explode',
+        labelInterpolationFnc: function(value) {
+          return value;
+        }
+      }],
+      ['screen and (min-width: 1024px)', {
+        labelOffset: 80,
+        chartPadding: 20
+      }]
+    ];
+
+    new Chartist.Pie('.ct-chart', data, options, responsiveOptions);
     });
 
-})
 }

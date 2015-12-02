f3cf862530f29eebee1ee17f4b7aff0dd96d327d
Alex Hoffnung <alex.hoffnung@gmail.com>
2015-12-02 14:22:08 -0500

    Add baby template. Begin modeling baby object.

---
+++ client/baby/baby.js
@@ -0,0 +1,52 @@
+if (Meteor.isClient) {
+	// This code only runs on the client
+	Meteor.subscribe("babies");
+
+	Template.baby.helpers({
+	  userId: function() {
+	    return Meteor.userId();
+	  },
+	  username: function() {
+	    return Meteor.user().username;
+	  },
+	  firstName: function() {
+	    return Meteor.user().profile.firstName;
+	  },
+	  lastName: function() {
+	    return Meteor.user().profile.lastName;
+	  },
+    streetAddress: function() {
+      return Meteor.user().profile.streetAddress;
+    }
+	});
+
+
+	Template.baby.events({
+    /*  
+    "submit .update-user-lastname": function (event) {
+      // Prevent default browser form submit
+      event.preventDefault();
+
+      // Get value from form element
+      var lastNameText = event.target.lastNameText.value;
+      // Update current user last name field
+      Meteor.users.update({_id:Meteor.userId()}, { $set: {'profile.lastName':lastNameText} });
+ 
+      // Clear form
+      event.target.lastNameText.value = "";
+    },
+    "submit .update-user-firstname": function (event) {
+      // Prevent default browser form submit
+      event.preventDefault();
+
+      // Get value from form element
+      var firstNameText = event.target.firstNameText.value;
+      // Update current user firstname field
+      Meteor.users.update({_id:Meteor.userId()}, { $set: {'profile.firstName':firstNameText} });
+ 
+      // Clear form
+      event.target.firstNameText.value = "";
+    }
+    */
+  });
+}

1fb9c91b2c0136235f6686f47e909718a85f794c
Alex Hoffnung <alex.hoffnung@gmail.com>
2016-01-02 21:20:59 -0500

    Update meal chart to show ounces per day for previous week.

--- client/charts/charts.js
+++ client/charts/charts.js
@@ -34,6 +34,45 @@ Template.charts.onRendered(function () {
     // Get current user id
     var currentUserId = Meteor.userId();
     var activeBaby = Session.get("activeBaby");
+
+    var today = new Date();
+//
+    var mealWeek = [
+      moment().add(-7,'days').startOf('day'),
+      moment().add(-6,'days').startOf('day'),
+      moment().add(-5,'days').startOf('day'),
+      moment().add(-4,'days').startOf('day'),
+      moment().add(-3,'days').startOf('day'),
+      moment().add(-2,'days').startOf('day'),
+      moment().add(-1,'days').startOf('day')
+    ];
+
+    var dayOneOunceTwo = Meals.find({ $and: [ {ounces:"2"}, {createdAtStart:mealWeek[0]._d}, { owner:currentUserId }, { babyName:activeBaby }] }).count();
+    var dayTwoOunceTwo = Meals.find({ $and: [ {ounces:"2"}, {createdAtStart:mealWeek[1]._d}, { owner:currentUserId }, { babyName:activeBaby }] }).count();
+    var dayThreeOunceTwo = Meals.find({ $and: [ {ounces:"2"}, {createdAtStart:mealWeek[2]._d}, { owner:currentUserId }, { babyName:activeBaby }] }).count();
+    var dayFourOunceTwo = Meals.find({ $and: [ {ounces:"2"}, {createdAtStart:mealWeek[3]._d}, { owner:currentUserId }, { babyName:activeBaby }] }).count();
+    var dayFiveOunceTwo = Meals.find({ $and: [ {ounces:"2"}, {createdAtStart:mealWeek[4]._d}, { owner:currentUserId }, { babyName:activeBaby }] }).count();
+    var daySixOunceTwo = Meals.find({ $and: [ {ounces:"2"}, {createdAtStart:mealWeek[5]._d}, { owner:currentUserId }, { babyName:activeBaby }] }).count();
+    var daySevenOunceTwo = Meals.find({ $and: [ {ounces:"2"}, {createdAtStart:mealWeek[6]._d}, { owner:currentUserId }, { babyName:activeBaby }] }).count();
+
+    var dayOneOunceFour = Meals.find({ $and: [ {ounces:"4"}, {createdAtStart:mealWeek[0]._d}, { owner:currentUserId }, { babyName:activeBaby }] }).count();
+    var dayTwoOunceFour = Meals.find({ $and: [ {ounces:"4"}, {createdAtStart:mealWeek[1]._d}, { owner:currentUserId }, { babyName:activeBaby }] }).count();
+    var dayThreeOunceFour = Meals.find({ $and: [ {ounces:"4"}, {createdAtStart:mealWeek[2]._d}, { owner:currentUserId }, { babyName:activeBaby }] }).count();
+    var dayFourOunceFour = Meals.find({ $and: [ {ounces:"4"}, {createdAtStart:mealWeek[3]._d}, { owner:currentUserId }, { babyName:activeBaby }] }).count();
+    var dayFiveOunceFour = Meals.find({ $and: [ {ounces:"4"}, {createdAtStart:mealWeek[4]._d}, { owner:currentUserId }, { babyName:activeBaby }] }).count();
+    var daySixOunceFour = Meals.find({ $and: [ {ounces:"4"}, {createdAtStart:mealWeek[5]._d}, { owner:currentUserId }, { babyName:activeBaby }] }).count();
+    var daySevenOunceFour = Meals.find({ $and: [ {ounces:"4"}, {createdAtStart:mealWeek[6]._d}, { owner:currentUserId }, { babyName:activeBaby }] }).count();
+
+    var dayOneOunceSix = Meals.find({ $and: [ {ounces:"6"}, {createdAtStart:mealWeek[0]._d}, { owner:currentUserId }, { babyName:activeBaby }] }).count();
+    var dayTwoOunceSix = Meals.find({ $and: [ {ounces:"6"}, {createdAtStart:mealWeek[1]._d}, { owner:currentUserId }, { babyName:activeBaby }] }).count();
+    var dayThreeOunceSix = Meals.find({ $and: [ {ounces:"6"}, {createdAtStart:mealWeek[2]._d}, { owner:currentUserId }, { babyName:activeBaby }] }).count();
+    var dayFourOunceSix = Meals.find({ $and: [ {ounces:"6"}, {createdAtStart:mealWeek[3]._d}, { owner:currentUserId }, { babyName:activeBaby }] }).count();
+    var dayFiveOunceSix = Meals.find({ $and: [ {ounces:"6"}, {createdAtStart:mealWeek[4]._d}, { owner:currentUserId }, { babyName:activeBaby }] }).count();
+    var daySixOunceSix = Meals.find({ $and: [ {ounces:"6"}, {createdAtStart:mealWeek[5]._d}, { owner:currentUserId }, { babyName:activeBaby }] }).count();
+    var daySevenOunceSix = Meals.find({ $and: [ {ounces:"6"}, {createdAtStart:mealWeek[6]._d}, { owner:currentUserId }, { babyName:activeBaby }] }).count();
+
+
+
     var diaperData = {
     labels: ['wet', 'dirty'],
     series: [[
@@ -42,11 +81,23 @@ Template.charts.onRendered(function () {
     ]]
     };
     var mealData = {
-    labels: ['2oz', '4oz','6oz'],
+    labels: [
+      mealWeek[0].format('ddd'),
+      mealWeek[1].format('ddd'),
+      mealWeek[2].format('ddd'),
+      mealWeek[3].format('ddd'),
+      mealWeek[4].format('ddd'),
+      mealWeek[5].format('ddd'),
+      mealWeek[6].format('ddd')
+    ],
     series: [[
-        Meals.find( { $and: [ {ounces:"2"}, {babyName: activeBaby}, { owner:currentUserId } ] } ).count(),
-        Meals.find( { $and: [ {ounces:"4"}, {babyName: activeBaby}, { owner:currentUserId } ] } ).count(),
-        Meals.find( { $and: [ {ounces:"6"}, {babyName: activeBaby}, { owner:currentUserId } ] } ).count()
+        (2 * dayOneOunceTwo) + (4 * dayOneOunceFour) + (6 * dayOneOunceSix),
+        (2 * dayTwoOunceTwo) + (4 * dayTwoOunceFour) + (6 * dayTwoOunceSix),
+        (2 * dayThreeOunceTwo) + (4 * dayThreeOunceFour) + (6 * dayThreeOunceSix),
+        (2 * dayFourOunceTwo) + (4 * dayFourOunceFour) + (6 * dayFourOunceSix),
+        (2 * dayFiveOunceTwo) + (4 * dayFiveOunceFour) + (6 * dayFiveOunceSix),
+        (2 * daySixOunceTwo) + (4 * daySixOunceFour) + (6 * daySixOunceSix),
+        (2 * daySevenOunceTwo) + (4 * daySevenOunceFour) + (6 * daySevenOunceSix),
 
     ]]
     };

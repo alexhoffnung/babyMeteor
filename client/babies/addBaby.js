e53c1a52484e5633d6074002f48bfa4ade303574
Alex Hoffnung <alex.hoffnung@gmail.com>
2016-01-03 23:49:22 -0500

    Update add baby autoform with autovalues.

--- client/babies/addBaby.js
+++ client/babies/addBaby.js
@@ -1,13 +1,18 @@
-Template.navbar.onRendered(function () {
+Template.addBaby.onRendered(function () {
 	// set focus when modal is opened
 	$('#modal-content').on('shown.bs.modal', function () {
 	    $("#txtname").focus();
 	});
 
-	// show the modal onload
-	$('#modal-content').modal({
-	    show: true
-	});
+	var currentUserId = Meteor.userId();
+	var babyCount = Babies.find({owner:currentUserId}).count();
+
+	if (babyCount === 0) {
+		// show the modal onload
+		$('#modal-content').modal({
+		    show: true
+		});
+	}
 
 	// everytime the button is pushed, open the modal, and trigger the shown.bs.modal event
 	$('#openBtn').click(function () {
--- client/babies/baby-profile.js
+++ client/babies/baby-profile.js
@@ -29,33 +29,112 @@ if (Meteor.isClient) {
 
 /**********************************/
 
+Schemas.babyInformation = new SimpleSchema({
+  babyName:{
+    type: String,
+    label: 'First name'//,
+  },
+  dob: {
+    type: Date,
+    label: 'Date of birth'
+  },
+  gender: {
+    type: String,
+    label: 'Sex of baby'
+  },
+  allergies: {
+    type: String,
+    label: 'Baby allergies'
+  },
+  activeState: {
+    type: Boolean,
+    label: 'Active baby',
+    optional:true,
+    autoValue:function(){
+        return true;
+    }
+  },
+  createdAt: {
+    type: Date,
+    label: 'Created date',
+    optional:true,
+    autoValue:function(){
+        return new Date();
+    }
+  },
+  owner: {
+    type: String,
+    label: 'Baby creator',
+    optional:true,
+    autoValue:function(){
+        return Meteor.userId();
+    }
+  },
+  username: {
+    type: String,
+    label: 'Baby creator name',
+    optional:true,
+    autoValue:function(){
+        return Meteor.userId().username;
+    }
+  }
+});
 
 Schemas.contactInformation = new SimpleSchema({
   firstname:{
     type: String,
     label: 'First name',
-    autoform: {
-      afFieldInput: {
-        type: "contenteditable"
-      }
-    }
+    optional: true
+//    autoform: {
+//      afFieldInput: {
+//        type: "contenteditable"
+//      }
+//    }
   },
   lastname: {
     type: String,
-    label: 'Last name'
+    label: 'Last name',
+    optional: true
   }
 });
 
 Schemas.addressInformation = new SimpleSchema({
   address:{
     type: String,
-    label: 'Street Address'
+    label: 'Street Address',
+    optional: true
+  },
+  city:{
+    type: String,
+    label: 'City',
+    optional: true
+  },
+  state:{
+    type: String,
+    label: 'State',
+    optional: true
+  },
+  zip:{
+    type: String,
+    label: 'Zip Code',
+    optional: true
+  }
+});
+
+Schemas.imageInformation = new SimpleSchema({
+  image:{
+    type: Object,
+    label: 'Baby Image',
+    blackbox: true,
+    optional: true
   }
 });
 
 Babies.attachSchema([
+  Schemas.babyInformation,
   Schemas.contactInformation,
-  Schemas.addressInformation
+  Schemas.addressInformation,
+  Schemas.imageInformation,
 ]);
 
 

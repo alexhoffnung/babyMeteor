42298e9e7582f721f956dc33b0fd7752f6959d82
Alex Hoffnung <alex.hoffnung@gmail.com>
2016-01-11 14:52:05 -0500

    Update collection publish statements.

--- server/collections/collections.js
+++ server/collections/collections.js
@@ -1,69 +1,27 @@
-/*********************************
-*
-*   Publish collections
-*
-*********************************/
+Meteor.publish("tasks", function () {
+  return Tasks.find({ owner: this.userId });
+});
 
+Meteor.publish("meals", function () {
+  return Meals.find({ owner: this.userId });
+});
 
-  Meteor.publish("tasks", function () {
-    return Tasks.find({
-      $or: [
-        { private: {$ne: true} },
-        { owner: this.userId }
-      ]
-    });
-  });
+Meteor.publish("diapers", function () {
+  return Diapers.find({ owner: this.userId });
+});
 
-  Meteor.publish("meals", function () {
-    return Meals.find({
-      $or: [
-        { private: {$ne: true} },
-        { owner: this.userId }
-      ]
-    });
-  });
+Meteor.publish("sleeps", function () {
+  return Sleeps.find({ owner: this.userId });
+});
 
+Meteor.publish("calendarEvent", function () {
+  return CalendarEvent.find({ owner: this.userId });
+});
 
-  Meteor.publish("diapers", function () {
-    return Diapers.find({
-      $or: [
-        { private: {$ne: true} },
-        { owner: this.userId }
-      ]
-    });
-  });
+Meteor.publish("caretakers", function () {
+  return Caretakers.find({ owner: this.userId });
+});
 
-  Meteor.publish("sleeps", function () {
-    return Sleeps.find({
-      $or: [
-        { private: {$ne: true} },
-        { owner: this.userId }
-      ]
-    });
-  });
-
-  Meteor.publish("calendarEvent", function () {
-    return CalendarEvent.find({
-      $or: [
-        { private: {$ne: true} },
-        { owner: this.userId }
-      ]
-    });
-  });
-
-  Meteor.publish("caretakers", function () {
-    return Caretakers.find({
-      $or: [
-        { owner: this.userId }
-      ]
-    });
-  });
-
-  Meteor.publish("babies", function () {
-    return Babies.find({
-      $or: [
-        { private: {$ne: true} },
-        { owner: this.userId }
-      ]
-    });
-  });
+Meteor.publish("babies", function () {
+  return Babies.find({ owner: this.userId });
+});

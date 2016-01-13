31094ab9c091d972533d2411a3a6fe7ceee51c8c
Alex Hoffnung <alex.hoffnung@gmail.com>
2016-01-12 22:23:53 -0500

    Add baby image to baby card.

--- client/babies/babyCard.js
+++ client/babies/babyCard.js
@@ -10,5 +10,9 @@ Template.babyCard.helpers({
     	]
     });
     return Babies.findOne({ $and: [{owner: currentUserId},{_id:activeBaby._id}]});
+  },
+  images: function () {
+      var activeBaby = Babies.findOne({ $and:[{owner:Meteor.userId()},{activeState:true}]});
+    return Images.find({'metadata.babyId':activeBaby._id}); // Where Images is an FS.Collection instance
   }
 });

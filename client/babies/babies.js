f43c2d41d41e2cd85dca15de6bb2f63a0a336f00
Alex Hoffnung <alex.hoffnung@gmail.com>
2016-01-15 10:15:17 -0500

    Fix setActiveBaby bug when user has single baby.

--- client/babies/babies.js
+++ client/babies/babies.js
@@ -1,14 +1,15 @@
 Meteor.subscribe("babies");
 
 Meteor.startup(function () {
-	var newActiveBaby
 
 	var currentUserId = Meteor.userId();
 	var babyCount = Babies.find({owner:currentUserId}).count();
-
+console.log("random baby 2")
 	if (babyCount > 0) {
 		var activeBaby = Babies.findOne({$and:[{owner:currentUserId},{activeState:true}]});
+		console.log(activeBaby)
 		if(! activeBaby) {
+			console.log("random baby")
 			Meteor.call("setRandomBaby");//UPDATE 
 		}
 	}
--- server/babies/babies.js
+++ server/babies/babies.js
@@ -4,49 +4,51 @@ Babies.allow({
   remove: function () { return true; }
 });
 
-  Meteor.methods({
-    setRandomBaby: function() {
-      var activeBaby = Babies.findOne({owner:currentUserId});
-      Babies.update({ babyId: activeBaby._id }, { $set: { activeState: true } });
-    },
-    updateActiveState: function() {
-      Babies.update({ activeState: true }, { $set: { activeState: false } });
-    },
-    deleteBaby: function (babyId) {
-    var baby = Babies.findOne(babyId);
-    var currentUserId = Meteor.userId();
-    if (baby.owner !== currentUserId) {
-      throw new Meteor.Error("not-authorized");
-    }
- 
-    Babies.remove(babyId);
-    //Clean collections
-    Meals.remove({babyId:babyId});
-    Diapers.remove({babyId:babyId});
-    Sleeps.remove({babyId:babyId});
-
-    var baby = Babies.findOne({ 
-      $and:[
-        {activeState:true},
-        {owner:currentUserId}
-      ]
-    });
-    if(!baby){
-      baby = Babies.findOne({owner:currentUserId});
-      if(baby){
-        Babies.update(baby._id,{$set:{activeState:true}});
-      }
-      else{
-        // ADD BABY!!!
-      }
-    }
+Meteor.methods({
+  setRandomBaby: function() {
+    console.log("in setRandomBaby")
+    var activeBaby = Babies.findOne({owner:currentUserId});
+    Babies.update({ babyId: activeBaby._id }, { $set: { activeState: true } });
   },
-  setActiveBaby: function (babyId) {
-    var currentUserId = Meteor.userId();
-    var prevBaby = Babies.findOne({$and:[{owner:currentUserId},{activeState:true}]});
-
-    Babies.update(prevBaby._id, { $set: { activeState: false } });
-    Babies.update(babyId, { $set: { activeState: true } });
+  updateActiveState: function() {
+    Babies.update({ activeState: true }, { $set: { activeState: false } });
+  },
+  deleteBaby: function (babyId) {
+  var baby = Babies.findOne(babyId);
+  var currentUserId = Meteor.userId();
+  if (baby.owner !== currentUserId) {
+    throw new Meteor.Error("not-authorized");
   }
 
+  Babies.remove(babyId);
+  //Clean collections
+  Meals.remove({babyId:babyId});
+  Diapers.remove({babyId:babyId});
+  Sleeps.remove({babyId:babyId});
+
+  var baby = Babies.findOne({ 
+    $and:[
+      {activeState:true},
+      {owner:currentUserId}
+    ]
   });
+  if(!baby){
+    baby = Babies.findOne({owner:currentUserId});
+    if(baby){
+      Babies.update(baby._id,{$set:{activeState:true}});
+    }
+    else{
+      // ADD BABY!!!
+    }
+  }
+},
+setActiveBaby: function (babyId) {
+  var currentUserId = Meteor.userId();
+  var prevBaby = Babies.findOne({$and:[{owner:currentUserId},{activeState:true}]});
+  if(prevBaby) {
+    Babies.update(prevBaby._id, { $set: { activeState: false } });
+  }
+  Babies.update(babyId, { $set: { activeState: true } });
+}
+
+});

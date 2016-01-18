643e6c9f6f9c4753422423b239c9f94d130a4064
Alex Hoffnung <alex.hoffnung@gmail.com>
2016-01-18 14:31:16 -0500

    Add custom sAlert template with spacebars.

--- client/salert/salert.js
+++ client/salert/salert.js
@@ -2,26 +2,45 @@ Template.alert.events({
     'click .alert-click-class': function (e) {
         e.preventDefault();
         console.log('Alert link clicked!');
-        sAlert.success('Please register', {timeout: 'none'});
+        sAlert.success('Last Meal: <a href="/babies" class="alert-click-class">{{incompleteCount}}</a>');
     }
 });
 
-
+Template.sAlertCustom.helpers({
+  "incompleteCount": function () {
+    var currentUserId = Meteor.userId();
+    var activeBaby = Babies.findOne({
+      $and: [
+        {owner:currentUserId},
+        {activeState:true}
+      ]
+    });
+    var today = moment().add(-1,'days');
+    return Meals.find( 
+      { $and: [ 
+        {createdAt: {$gte: today._d}},
+        {babyId:activeBaby._id},
+        {owner:currentUserId}
+        ] 
+      } 
+    ).count();
+  }
+});
 Meteor.startup(function () {
 
     sAlert.config({
-        effect: '',
-        position: 'top-right',
-        timeout: 5000,
-        html: false,
+        effect: 'genie',
+        position: 'top',
+        timeout: 3000,
+        html: true,
         onRouteClose: true,
-        stack: true,
+        stack: false,
         // or you can pass an object:
         // stack: {
         //     spacing: 10 // in px
         //     limit: 3 // when fourth alert appears all previous ones are cleared
         // }
-        offset: 0, // in px - will be added to first alert (bottom or top - depends of the position in config)
+        offset: 150, // in px - will be added to first alert (bottom or top - depends of the position in config)
         beep: false,
         // examples:
         // beep: '/beep.mp3'  // or you can pass an object:
